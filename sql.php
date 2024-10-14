cf7cb7c165d064622fbb14255c3b0a6f47c546bb
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-10-14 12:13:11 -0600

    First Commit

---
+++ sql.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV["HOST"];
+$user = $_ENV["USER"];
+$password = $_ENV["PASSWORD"];
+$db = $_ENV["DB"];
+
+$conn = new mysqli($host, $user, $password, $db);
