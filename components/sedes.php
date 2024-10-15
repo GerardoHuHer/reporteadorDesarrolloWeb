9c133ff184f5ec27c26ecc8392c991f31b6ee814
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-10-14 21:37:13 -0600

    Modifications

--- components/sedes.php
+++ components/sedes.php
@@ -1,8 +1,8 @@
-<select class="talent" name="talent" id="talent" >
+<select name="sede" id="sede">
 
 </select>
 
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
-<!-- <script src="../assets/js/talent.js"></script> -->
+<!-- <script src="../assets/js/sede.js"></script> -->
--- index.php
+++ index.php
@@ -8,7 +8,13 @@
 </head>
 
 <body>
-    hola
+    <?php include "./components/sedes.php"; ?>
+    <?php include "./components/talent.php"; ?>
+
 </body>
 
 </html>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+<script src="./assets/js/sede.js"></script>
+<script src="./assets/js/talent.js"></script>
