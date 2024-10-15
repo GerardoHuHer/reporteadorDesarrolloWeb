d73c97c92a562b5cebfdd0e205f96acfb54a4594
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-10-14 18:45:06 -0600

    sql.php modifications

--- sql.php
+++ sql.php
@@ -5,9 +5,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+
+
 $host = $_ENV["HOST"];
 $user = $_ENV["USER"];
 $password = $_ENV["PASSWORD"];
 $db = $_ENV["DB"];
+
 
 $conn = new mysqli($host, $user, $password, $db);
