f555992193905024993ba217ac01fbc116269d4f
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-10-15 18:53:07 -0600

    Se añade funcionalidad de los selects en un archivo prueba

---
+++ components/prueba.php
@@ -0,0 +1,12 @@
+
+<select name="prueba" id="prueba" onclick="prueba()">
+
+</select>
+
+<div id="lista-prueba">
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+<script src="../assets/js/prueba.js"></script>
